ed5e8445f04aa6aacbc6e4ed16829e507219b6d0
HasprunLukas <test>
2023-03-01 18:11:27 +0100

    completed 2nd task

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,15 @@
 from ximea import xiapi
 import cv2
 import numpy as np
-import glob
-import time
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((5 * 7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:5].T.reshape(-1, 2)
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
 
 ### runn this command first echo 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mb  ###
 
@@ -13,13 +20,23 @@ def open_image(path):
     img = cv2.imread(path)
     cv2.imshow('test', img)
 
+def print_intrinsic_properties(gray):
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    fx = mtx[0][0]
+    fy = mtx[1][1]
+    cx = mtx[0][2]
+    cy = mtx[1][2]
+    print("fx : " + str(fx))
+    print("fy : " + str(fy))
+    print("cx : " + str(cx))
+    print("cy : " + str(cy))
+    print(mtx)
+
 def test_function(name):
     img = cv2.imread(name)
-    cv2.imshow('testBefore',img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('testAfter', gray)
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (5, 7), None)
     # If found, add object points, image points (after refining them)
     print(ret)
     if ret == True:
@@ -27,86 +44,60 @@ def test_function(name):
         corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv2.drawChessboardCorners(img, (7, 5), corners2, ret)
         cv2.imshow('img', img)
+        print_intrinsic_properties(gray)
         cv2.waitKey(500)
 
-#create instance for first connected camera
-cam = xiapi.Camera()
+def main():
+    # create instance for first connected camera
+    cam = xiapi.Camera()
 
+    # start communication
+    # to open specific device, use:
+    # cam.open_device_by_SN('41305651')
+    # (open by serial number)
+    print('Opening first camera...')
+    cam.open_device()
+    # settings
+    cam.set_exposure(10000)
+    cam.set_param("imgdataformat", "XI_RGB32")
+    cam.set_param("auto_wb", 1)
 
+    print('Exposure was set to %i us' % cam.get_exposure())
 
-#start communication
-#to open specific device, use:
-#cam.open_device_by_SN('41305651')
-#(open by serial number)
-print('Opening first camera...')
-cam.open_device()
+    # create instance of Image to store image data and metadata
+    img = xiapi.Image()
 
-#settings
-cam.set_exposure(10000)
-cam.set_param("imgdataformat","XI_RGB32")
-cam.set_param("auto_wb",1)
+    # start data acquisition
+    print('Starting data acquisition...')
+    cam.start_acquisition()
 
-print('Exposure was set to %i us' %cam.get_exposure())
-
-#create instance of Image to store image data and metadata
-img = xiapi.Image()
-
-#start data acquisition
-print('Starting data acquisition...')
-cam.start_acquisition()
-
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
-key = cv2.waitKey()
-while key != ord('q'):
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv2.resize(image,(240,240))
-    # image = cv2.imread('chessboard_BKP.jpg')
-    cv2.imshow("test", image)
-    # if key == ord('c'):
-    #     get_chessboard(image)
-    # if key == ord('p'):
-    #     open_image('chessboard_BKP.jpg')
-    if key == ord('t'):
-        test_function('chessboard_BKP.jpg')
     key = cv2.waitKey()
+    while key != ord('q'):
+        cam.get_image(img)
+        image = img.get_image_data_numpy()
+        image = cv2.resize(image, (240, 240))
+        # image = cv2.imread('chessboard_BKP.jpg')
+        cv2.imshow("test", image)
+        # if key == ord('c'):
+        #     get_chessboard(image)
+        # if key == ord('p'):
+        #     open_image('chessboard_BKP.jpg')
+        if key == ord('t'):
+            test_function('chessboard_BKP.jpg')
+        key = cv2.waitKey()
 
-# for i in range(10):
-#     #get data and pass them from camera to img
-#     cam.get_image(img)
-#     image = img.get_image_data_numpy()
-#     cv2.imshow("test", image)
-#     cv2.waitKey()
-#     #get raw data from camera
-#     #for Python2.x function returns string
-#     #for Python3.x function returns bytes
-#     data_raw = img.get_image_data_raw()
-#
-#     #transform data to list
-#     data = list(data_raw)
-#
-#     #print image data and metadata
-#     print('Image number: ' + str(i))
-#     print('Image width (pixels):  ' + str(img.width))
-#     print('Image height (pixels): ' + str(img.height))
-#     print('First 10 pixels: ' + str(data[:10]))
-#     print('\n')
+    # stop data acquisition
+    print('Stopping acquisition...')
+    cam.stop_acquisition()
 
-#stop data acquisition
-print('Stopping acquisition...')
-cam.stop_acquisition()
+    # stop communication
+    cam.close_device()
 
-#stop communication
-cam.close_device()
+    print('Done.')
 
-print('Done.')
+
+if __name__ == "__main__":
+    main()
+
